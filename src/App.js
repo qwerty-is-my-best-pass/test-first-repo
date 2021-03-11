2d6f797fdf5228ebeeef764488b2839fecf91014
qwerty-is-my-best-pass <qwerty.is.my.best.pass@gmail.com>
2021-03-11 22:09:59 +0500

    so amazing

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import AwesomeComponent from './AwesomeComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <AwesomeComponent text='А это первый репозиторий'/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
